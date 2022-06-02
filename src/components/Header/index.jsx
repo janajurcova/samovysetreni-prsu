2852e247c82c4ac76bbabb0aaa3356d48c143c61
Jana Jurčová <jj.jurcova@gmail.com>
2022-06-02 16:24:14 +0200

    correct className + new container in calendar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,11 +6,11 @@ export const Header = () => (
     <header className="page-header">
         <div className="container" style={{ display: 'flex' }}>
             <div className="logo" src="./img/Blogo-transparent.png">
-            <div class="theme-switch-wrapper">
+            <div className="theme-switch-wrapper">
                 <span>gender mode</span>
-                <label class="theme-switch" for="checkbox">
-                    <input type="checkbox" id="checkbox" />
-                    <div class="slider round"></div>
+                <label className="theme-switch" for="checkbox">
+                    <input typeName="checkbox" id="checkbox" />
+                    <div className="slider round"></div>
                 </label>
                
             <div className="navigation">
--- src/pages/Calendar/index.jsx
+++ src/pages/Calendar/index.jsx
@@ -2,87 +2,93 @@ import React from 'react';
 import './style.css';
 
 export const Calendar = () => {
-
-
     return (
         <main style={{ padding: '1rem' }}>
             <h1>Kalendář</h1>
-                        <div class="container1">
-                            <div class="calendar">
+            <div className="container1">
+                <div className="calendar">
 
-                                <header>
-                                    <h2>April</h2>
+                    <header>
+                        <h2>April</h2>
+                        <a className="btn-prev fontawesome-angle-left" href="#"></a>
+                        <a className="btn-next fontawesome-angle-right" href="#"></a>
+                    </header>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Po</td>
+                                <td>Ut</td>
+                                <td>St</td>
+                                <td>Ct</td>
+                                <td>Pa</td>
+                                <td>So</td>
+                                <td>Ne</td>
+                            </tr>
+                        </thead>
 
-                                    <a class="btn-prev fontawesome-angle-left" href="#"></a>
-                                    <a class="btn-next fontawesome-angle-right" href="#"></a>
+                        <tbody>
+                            <tr>
+                                <td className="prev-month">29</td>
+                                <td className="prev-month">30</td>
+                                <td className="prev-month">31</td>
 
-                                </header>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <td>Po</td>
-                                            <td>Ut</td>
-                                            <td>St</td>
-                                            <td>Ct</td>
-                                            <td>Pa</td>
-                                            <td>So</td>
-                                            <td>Ne</td>
-                                        </tr>
-                                    </thead>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>6</td>
+                                <td>7</td>
+                                <td>8</td>
+                                <td className="event">9</td>
+                                <td className="current-day event">10</td>
+                                <td>11</td>
+                            </tr>
+                            <tr>
+                                <td>12</td>
+                                <td>13</td>
+                                <td>14</td>
+                                <td>15</td>
+                                <td>16</td>
+                                <td>17</td>
+                                <td>18</td>
+                            </tr>
+                            <tr>
+                                <td>19</td>
+                                <td>20</td>
+                                <td>21</td>
+                                <td className="event">22</td>
+                                <td>23</td>
+                                <td>24</td>
+                                <td>25</td>
+                            </tr>
 
-                                    <tbody>
+                            <tr>
+                                <td>26</td>
+                                <td>27</td>
+                                <td>28</td>
+                                <td>29</td>
+                                <td>30</td>
+                                <td className="next-month">1</td>
+                                <td className="next-month">2</td>
+                            </tr>
+                        </tbody>
+                    </table>
 
-                                        <tr>
-                                            <td class="prev-month">29</td>
-                                            <td class="prev-month">30</td>
-                                            <td class="prev-month">31</td>
+                </div>
+                <div className="container2">
+                    <h4>Legenda:</h4>
+                </div>
+                <div className="container2">
+                    <h4>Nastavení samovyšetření:</h4>
+                </div>
+                <div className="container2">
+                    <h4>Nastavení diagnostického vyšetření:</h4>
+                </div>
+            </div>
 
-                                            <td>1</td>
-                                            <td>2</td>
-                                            <td>3</td>
-                                            <td>4</td>
-                                        </tr>
-                                        <tr>
-                                            <td>5</td>
-                                            <td>6</td>
-                                            <td>7</td>
-                                            <td>8</td>
-                                            <td class="event">9</td>
-                                            <td class="current-day event">10</td>
-                                            <td>11</td>
-                                        </tr>
-                                        <tr>
-                                            <td>12</td>
-                                            <td>13</td>
-                                            <td>14</td>
-                                            <td>15</td>
-                                            <td>16</td>
-                                            <td>17</td>
-                                            <td>18</td>
-                                        </tr>
-                                        <tr>
-                                            <td>19</td>
-                                            <td>20</td>
-                                            <td>21</td>
-                                            <td class="event">22</td>
-                                            <td>23</td>
-                                            <td>24</td>
-                                            <td>25</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>26</td>
-                                            <td>27</td>
-                                            <td>28</td>
-                                            <td>29</td>
-                                            <td>30</td>
-                                            <td class="next-month">1</td>
-                                            <td class="next-month">2</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div> 
-                        </div> 
         </main>
     );
 };
