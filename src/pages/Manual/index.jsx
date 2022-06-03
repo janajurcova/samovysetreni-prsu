93983879ac8fec583fe39ff34da57e907444d099
Iveta Žižková <ziz.iveta@gmail.com>
2022-06-03 21:38:47 +0200

    Create carousel component

--- src/pages/Manual/index.jsx
+++ src/pages/Manual/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Carousel } from '../../components/Carousel';
 
 export const Manual = () => {
-    return (
-        <main style={{ padding: '1rem' }}>
-            <h1>Jak na to (.)(.)</h1>
-        </main>
-    );
+  return (
+    <main style={{ padding: '1rem' }}>
+      <h1>Jak na to (.)(.)</h1>
+      <Carousel />
+    </main>
+  );
 };
-
