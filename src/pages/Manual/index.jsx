f3068185207c0f50c99de059a4358e56ece2a79f
Iveta Žižková <ziz.iveta@gmail.com>
2022-06-04 21:59:00 +0200

    manual page

--- src/pages/Manual/index.jsx
+++ src/pages/Manual/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Carousel } from '../../components/Carousel';
+import pozor from './img/pozor-pink.png';
+import './style.css';
 
 export const Manual = () => {
   return (
     <main style={{ padding: '1rem' }}>
       <h1>Jak na to (.)(.)</h1>
       <Carousel />
+      <div className="manual__image">
+        <img className="manual__ifSomethingWrong" src={pozor} />
+      </div>
     </main>
   );
 };
