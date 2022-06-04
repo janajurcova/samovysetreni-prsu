bcbeb26b0059606fadf6479203deb26bce60a0c3
Iveta Žižková <ziz.iveta@gmail.com>
2022-06-04 23:10:35 +0200

    add If something wrong in manual

--- src/pages/Manual/index.jsx
+++ src/pages/Manual/index.jsx
@@ -6,10 +6,19 @@ import './style.css';
 export const Manual = () => {
   return (
     <main style={{ padding: '1rem' }}>
-      <h1>Jak na to (.)(.)</h1>
-      <Carousel />
-      <div className="manual__image">
-        <img className="manual__ifSomethingWrong" src={pozor} />
+      <div className="manual__container">
+        <h1>Jak na to (.)(.)</h1>
+        <Carousel />
+        <div className="manual__image">
+          <div className="manual__title">Co může být špatně?</div>
+          <div className="manual__text">
+            Bulka, jakýkoliv útvar, který předchozí měsíc v prsu hmatný nebyl,
+            vpáčená bradavka nebo kůže na prsu, výtok z bradavky, zčervenání
+            prsu, změna struktury kůže na prsu, krvácení z kůže okolo bradavky,
+            bolest prsu.{' '}
+          </div>
+          <img className="manual__ifSomethingWrong" src={pozor} />
+        </div>
       </div>
     </main>
   );
