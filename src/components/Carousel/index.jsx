e92c5d82452346b2feeffb023f6a1a2da5366d68
Iveta Žižková <ziz.iveta@gmail.com>
2022-06-04 15:39:38 +0200

    fixing carousel size

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react';
 import './carousel.css';
-import manual1 from './img/manual1.png';
-import manual2 from './img/manual2.png';
-import manual3 from './img/manual3.png';
-import manual4 from './img/manual4.png';
+import image1 from './img/1img.png';
+import image2 from './img/2img.png';
+import image3 from './img/3img.png';
+import image4 from './img/4img.png';
+import number1 from './img/1.png';
+import number2 from './img/2.png';
+import number3 from './img/3.png';
+import number4 from './img/4.png';
 
 const images = [
   {
     title: 'Vyšetřuj se pravidelně',
-    img: manual1,
+    img: image1,
+    number: number1,
     text: 'Vyšetřuj se každý měsíc. Vyšetřuj se sama. Proč? Protože Ty sama znáš svá prsa nejlépe. Kdy vyšetřovat? Ideálně 5. - 10. den menstruačního cyklu. Pokud menstruaci nemáš, stanov si jeden stejný den v měsíci.',
   },
   {
     title: 'Začni u zrcadla',
-    img: manual2,
+    img: image2,
+    number: number2,
     text: 'Podívej se na svá prsa v zrcadle. Zvedni ruce nad hlavu a prohlídni si je. Všímej si změny velikosti, tvaru prsu a tvaru bradavky, výtoku z bradavky nebo změny kůže na prsu.',
   },
   {
     title: 'Použij 3 prsty',
-    img: manual3,
+    img: image3,
+    number: number3,
     text: 'Každá máme jiný tvar prsu, proto je ideální vyšetřovat se vleže vždy třemi prsty, levou rukou pravý prs a naopak. Sáhni si na prsa třemi prsty naplocho a třemi krouživými pohyby vyšetřuj - nejprve povrchově, poté postupně přidávej tlak (na jednom místě celkem tři kroušžky). Nezapomeň, že takto je potřeba vyšetřit hrudník od podprsenky do podpaží, až ke klíční kosti. Snaž se nevynechat ani kousek. Po vyšetření celé plochy hrudníku nezapomeň na vyšetření podpaží.',
   },
   {
     title: 'Za měsíc znovu',
-    img: manual4,
+    img: image4,
+    number: number4,
     text: 'Je všechno v pořádku? Raduj se a mrkni na sebe zase za měsíc. Znpokojilo Tě něco? Nepanikař a zajdi za svým gynekologem. Každý útvar neznamená nutně nádor.',
   },
 ];
@@ -43,9 +51,17 @@ export const Carousel = () => {
           ←
         </button>
         <div className="carousel__media">
-          <img className="carousel__image" src={images[activePicture].img} />
-          <div className="carousel__title">{images[activePicture].title}</div>
-          <div className="carousel__text">{images[activePicture].text}</div>
+          <div className="carousel__images">
+            <img
+              className="carousel__number"
+              src={images[activePicture].number}
+            />
+            <img className="carousel__image" src={images[activePicture].img} />
+          </div>
+          <div className="carousel__text__content">
+            <div className="carousel__title">{images[activePicture].title}</div>
+            <div className="carousel__text">{images[activePicture].text}</div>
+          </div>
         </div>
         <button
           onClick={() => {
