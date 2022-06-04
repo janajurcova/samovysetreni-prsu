d82e137e54d5c7c39f3f42501ab2d2ca961d9f1c
Jana Jurčová <jj.jurcova@gmail.com>
2022-06-04 11:55:49 +0200

    homepage

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Header } from './components/Header';
 import { Questions } from './pages/Questions';
 import { Manual } from './pages/Manual';
 import { Home } from './pages/Home';
 import { Calendar } from './pages/Calendar';
 import './style.css';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => (
-  <body>
-  </body>
-)
+const App = () => {}
 
 
   createRoot(document.querySelector('#app')).render(
   <BrowserRouter>
-  <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/samovysetreni" element={<Manual />} />
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+import gender from './../../img/gender-icon.png'
+import icon1 from '../../img/Icons/Icons-flower.jpg'
+import icon2 from '../../img/Icons/Icons-cancer.jpg'
+import icon3 from '../../img/Icons/Icons-B.jpg'
+
+const Switcher = () => {
+    return (<div className="theme-switch-wrapper">
+        <img className="gender" src={gender} />
+        <label className="theme-switch" htmlFor="checkbox">
+            <input type="checkbox" id="checkbox" />
+            <div className="slider round"></div>
+        </label>
+    </div>)
+}
+
 
 export const Home = () => {
     return (
-        <main style={{ padding: '1rem', margin: '10rem' }}>
-            <h1>Home</h1>
-        </main>
+        <div className="home-page">
+            <header className="home-header">
+                <Switcher />
+                <div className="home-header__motto">
+                    <h1 className="home-header__title">Samovyšetření prsu</h1>
+                    <h4 className="home-header__subtitle">"TOUCH ME IF YOU CAN"</h4>
+                </div>
+            </header>
+            <main>
+                <div className="homepage-main">
+                    <div className="homepage-main__manual">
+                    <label>Jak na to (.)(.)</label>
+                        <Link
+                            style={{ display: 'block', margin: '1rem 0' }}
+                            to="/samovysetreni"
+                        >
+                            <img className="icon1" src={icon1} />
+                        </Link>{' '}
+                        
+                    </div>
+                    <div className="homepage-main__calendar">
+                        <label>Kalendář</label>
+                        <Link
+                            style={{ display: 'block', margin: '1rem 0' }}
+                            to="/kalendar"
+                        >
+                            <img className="icon2" src={icon2} />
+                        </Link>{' '}
+                        
+                    </div>
+                    <div className="homepage-main__questions">
+                        <label>Co dělat, když...</label>
+                        <Link
+                            style={{ display: 'block', margin: '1rem 0' }}
+                            to="/codelatkdyz"
+                        >
+                            <img className="icon3" src={icon3} />
+                        </Link>{' '}
+                        
+                    </div>
+                </div>
+            </main>
+        </div>
     );
 };
 
