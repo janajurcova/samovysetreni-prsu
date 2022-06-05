b34cd8702e9240a2b112f064f195d85bea660c79
Iveta Žižková <ziz.iveta@gmail.com>
2022-06-05 16:46:37 +0200

    styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,12 +5,16 @@ import { InstallButton } from '../InstallButton';
 export const Footer = () => {
   return (
     <>
-      <InstallButton />
-      <div className="footer ">
-        <small>
-          Copyright &copy; Bellis – mladé ženy s rakovinou prsu. Vyrobeno při
-          Digitální akademii Czechitas 2022
-        </small>
+      <div className="footer">
+        <div className="installButton">
+          <InstallButton />
+        </div>
+        <div className="bellis__footer">
+          <small>
+            Copyright &copy; Bellis – mladé ženy s rakovinou prsu. Vyrobeno při
+            Digitální akademii Czechitas 2022
+          </small>
+        </div>
       </div>
     </>
   );
